fc69afd6a2434b744fd96fffad02e368325890d0
gerardsimons <G.J.C.Simons@student.tudelft.nl>
2014-05-01 14:22:54 +0200

    Initial Commit

---
+++ Afstuderen/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Afstuderen
+//
+//  Created by Gerard Simons on 01/05/14.
+//  Copyright (c) 2014 Gerard Simons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
